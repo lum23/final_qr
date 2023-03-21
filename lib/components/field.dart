e3a88bd911a31ed3427fe395a6a93c64804469d8
Gap08 <cassylum23@gmail.com>
2023-03-21 11:13:29 +0800

    Update in Login Page

---
+++ lib/components/field.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+
+class FieldLogin extends StatefulWidget {
+  // ignore: prefer_typing_uninitialized_variables
+  final controller;
+  final String hintText;
+  final bool obscureText;
+  final Widget? prefixIcon;
+
+  const FieldLogin({
+    super.key, 
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+    this.prefixIcon
+  });
+
+  @override
+  State<FieldLogin> createState() => _FieldLoginState();
+}
+
+class _FieldLoginState extends State<FieldLogin> {
+  bool x = false;
+  Widget visible = const Icon(Icons.visibility,color: Colors.black87);
+  void obsc(){
+    x = widget.obscureText;
+  }
+
+  void toggleVisibility(){
+    if (x == true){
+      setState(() {
+        visible = const Icon(Icons.visibility_off,color: Colors.black87);
+      });
+    }
+    else{
+      setState(() {
+        visible = const Icon(Icons.visibility,color: Colors.black87);
+      });
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: widget.obscureText 
+      ? TextField( // TRUE CONDITION
+        controller: widget.controller,
+        obscureText: x,
+        decoration: InputDecoration(
+          prefixIcon: widget.prefixIcon,
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade400),
+          ),
+          fillColor: Colors.grey.shade200,
+          filled: true,
+          hintText: widget.hintText,
+          suffixIcon: Align(
+            widthFactor: 1.0,
+            heightFactor: 1.0,
+            child: GestureDetector(
+              onTap: () {
+                setState((){
+                  x = !x;
+                  toggleVisibility();               
+                });
+              },
+              child: visible
+            ),
+          ),
+        ),
+      ) 
+      : TextField( // FALSE CONDITION
+        controller: widget.controller,
+        obscureText: widget.obscureText,
+        decoration: InputDecoration(
+          prefixIcon: widget.prefixIcon,
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade400),
+          ),
+          fillColor: Colors.grey.shade200,
+          filled: true,
+          hintText: widget.hintText,
+        ),
+      ),
+    );
+  }
+}
+
+// class FieldLogin extends StatefulWidget{
+
+//   @override
+//   FieldLoginState createState() => FieldLoginState();
+  
+// }
+// class FieldLoginState extends State<FieldLogin>{
+//   // ignore: prefer_typing_uninitialized_variables
+//   final controller;
+//   final String hintText;
+//   late final bool obscuraText;
+//   final Widget? prefixIcon;
+
+//   FieldLoginState({
+//     required this.controller,
+//     required this.hintText,
+//     required this.obscuraText,
+//     this.prefixIcon,
+//   });
+
+//   @override
+//   Widget build(BuildContext context){
+//     // ignore: prefer_const_constructors
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(horizontal: 25),
+//       child: obscuraText ? TextField(
+//         controller: controller,
+//         obscureText: obscuraText,
+//         decoration: InputDecoration(
+//           prefixIcon: prefixIcon,
+//           enabledBorder: const OutlineInputBorder(
+//             borderSide: BorderSide(color: Colors.white),
+//           ),
+//           focusedBorder: OutlineInputBorder(
+//             borderSide: BorderSide(color: Colors.grey.shade400),
+//           ),
+//           fillColor: Colors.grey.shade200,
+//           filled: true,
+//           hintText: hintText,
+//           suffixIcon: Align(
+//             widthFactor: 1.0,
+//             heightFactor: 1.0,
+//             child: GestureDetector(
+//               onTap: () {
+//                 setState((){
+//                   obscuraText = !obscuraText;
+//                 });
+//               },
+//               child: const Icon(
+//                 Icons.visibility_off,
+//                 color: Colors.black87,
+//               ),
+//             ),
+//           ),
+//         ),
+//       ) : TextField(
+//         controller: controller,
+//         obscureText: obscuraText,
+//         decoration: InputDecoration(
+//           prefixIcon: prefixIcon,
+//           enabledBorder: const OutlineInputBorder(
+//             borderSide: BorderSide(color: Colors.white),
+//           ),
+//           focusedBorder: OutlineInputBorder(
+//             borderSide: BorderSide(color: Colors.grey.shade400),
+//           ),
+//           fillColor: Colors.grey.shade200,
+//           filled: true,
+//           hintText: hintText,
+//         ),
+//       ),
+//     );
+//   }
+  
+//   @override
+//   State<StatefulWidget> createState() {
+//     // TODO: implement createState
+//     throw UnimplementedError();
+//   }
+// }
---
+++ lib/pages/login.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:final_qr/components/field.dart';
+
+class LoginPage extends StatelessWidget{
+  LoginPage({super.key});
+
+  final userCont = TextEditingController();
+  final passCont = TextEditingController();
+  
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Color(0x6631ca52),
+                  Color(0x9931ca52),
+                  Color(0xcc31ca52),
+                  Color(0xff31ca52),
+                ]
+              )
+            ),
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: 100),
+                const Image
+                (
+                  image: AssetImage('assets/ndmu.png'),
+                  alignment: Alignment.center,
+                  fit: BoxFit.fitHeight,
+                  height: 200,
+                ),
+
+                const SizedBox(height: 70),
+                
+                FieldLogin( // USERNAME
+                  controller: userCont, 
+                  hintText: 'Username', 
+                  obscureText: false,
+                  prefixIcon: const Icon(
+                    Icons.person,
+                    color: Colors.black87,
+                  ),
+                ),
+                const SizedBox(height: 40),
+                FieldLogin(
+                  controller: passCont, 
+                  hintText: 'Password', 
+                  obscureText: true,
+                  prefixIcon: const Icon(
+                    Icons.lock,
+                    color: Colors.black87,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

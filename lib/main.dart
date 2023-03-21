583de7e0d11d59e39ca3cfe1f02f9f66d2345e25
Gap08 <cassylum23@gmail.com>
2023-03-21 11:11:37 +0800

    update_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
-import 'login.dart';
+import 'pages/login.dart';
 
 void main() 
 {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget 
-{
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) 
-  {
-    return const MaterialApp
-    (
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       title: 'Flutter Demo',
-      home: Login(),
+      home: LoginPage(),
     );
   }
 }
